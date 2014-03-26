35338dae31afb939c8ce71a76611863a77ea756f
Vincent BODIN <vbod@hotmail.com>
2014-03-26 10:33:37 +0100

    papiers à lire

--- code/test.py
+++ code/test.py
@@ -8,13 +8,29 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-image = 'DSCN2366.jpg'
+image = 'DSCN2369.jpg'
 img = load_image(image = image)
+edges = cv.Canny(img,10,150,apertureSize = 3)
 
-edges = cv.Canny(img,10,150)
-
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.subplot(131),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.subplot(132),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+lines = cv.HoughLines(edges,1,np.pi/180,200)
+for rho,theta in lines[0]:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+
+cv.imwrite('houghlines3.jpg',img)
+plt.subplot(133),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.show()
